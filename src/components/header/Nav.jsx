4a0fd010c578d56b8f357ede04f92c7eb5bb1366
Mona Khorasani <monakhrsn@gmail.com>
2024-10-24 10:45:35 +0200

    working on nav

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,38 +1,55 @@
+import { Navbar, Row, Col, FormCheck, NavbarBrand, Button, Image } from "react-bootstrap";
+import PrimaryButton from "../PrimaryButton";
 
 const Nav = () => {
   return (
-    <nav className="row align-items-center">
-      <a href="index.html" className="col-6 col-xl-2">
-        <img className="logo" src="/assets/images/logo.svg.svg" alt="Silicon" />
-        <span className="logo-text">Silicon</span>
-      </a>
-      <div className="featurs-link d-none d-xl-block col-xl-2 ">
-        <a href="#">Features</a>
-      </div>
-      <div className="col-6 col-xl-8">
-        <div className="row justify-content-end align-items-center">
-          <label htmlFor="flexSwitchCheckDefault" className="dark-mode-text d-none col-4 d-md-block text-md-end col-xl-2" aria-label="darkmode switch">Dark Mode</label>
-          <div className="col-3 col-xl-2 form-check form-switch">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="flexSwitchCheckDefault"
+    <Row>
+      <Navbar className="align-items-center">
+        <Col xs={6} xl={2}>
+          <NavbarBrand href="index.html">
+            <Image
+              className="logo"
+              src="/assets/images/logo.svg.svg"
+              alt="Silicon"
             />
-          </div>
-          <div className="col-2 d-xl-none ">
-            <div className="row d-flex justify-content-center justify-align-center">
-              <button className="navbar-toggle ">
-                <span className="navbar-toggle"></span>
-              </button>
-            </div>
-          </div> 
-          <button type="button" className="sign-btn transition d-none col-3 col-xl-2 d-xl-block">
-          Sign in / up 
-          </button>
-        </div>
-      </div>
-    </nav>
-  )
-}
+            <span className="logo-text">Silicon</span>
+          </NavbarBrand>
+        </Col>
+        <Col xl={2} className="featurs-link d-none d-xl-block ">
+          <a href="#">Features</a>
+        </Col>
+        <Col xs={6} xl={8}>
+          <Row className="justify-content-end align-items-center">
+            <label
+              htmlFor="flexSwitchCheckDefault"
+              className="dark-mode-text d-none col-4 d-md-block text-md-end col-xl-2"
+              aria-label="darkmode switch"
+            >
+              Dark Mode
+            </label>
+            <Col xs={3} xl={2}>
+              <FormCheck
+                className="form-check-input"
+                type="switch"
+                id="flexSwitchCheckDefault"
+              />
+            </Col>
+            <Col xs={2} className="d-xl-none">
+                <Button className="navbar-toggle ">
+                  <span className="navbar-toggle"></span>
+                </Button>
+            </Col>
+            <Col xl={2} className="d-none d-xl-block">
+            <PrimaryButton 
+            className="sign-btn transition"
+            text="Sign in / up"
+            />
+            </Col>
+          </Row>
+        </Col>
+      </Navbar>
+    </Row>
+  );
+};
 
 export default Nav;
