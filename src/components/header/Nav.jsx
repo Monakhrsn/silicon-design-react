fa7ee937a58274825bf211e7e96dc2884dc2c6fc
Mona Khorasani <monakhrsn@gmail.com>
2024-10-25 13:15:08 +0200

    Refactor nav

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,51 +1,68 @@
-import { Navbar, Row, Col, FormCheck, NavbarBrand, Button, Image } from "react-bootstrap";
+import {
+  Navbar,
+  Row,
+  Col,
+  Image,
+  Nav as BootstrapNav,
+} from "react-bootstrap";
 import PrimaryButton from "../PrimaryButton";
+import { useState } from "react";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 const Nav = () => {
+  const [expanded, setExpanded] = useState(false);
   return (
     <Row>
-      <Navbar className="align-items-center">
+      <Navbar
+        expanded={expanded}
+        expand="xl"
+        className="align-items-center"
+        onToggle={() => setExpanded(!expanded)}
+      >
         <Col xs={6} xl={2}>
-          <NavbarBrand href="index.html">
+          <Navbar.Brand href="index.html">
             <Image
               className="logo"
               src="/assets/images/logo.svg.svg"
               alt="Silicon"
             />
             <span className="logo-text">Silicon</span>
-          </NavbarBrand>
+          </Navbar.Brand>
         </Col>
-        <Col xl={2} className="featurs-link d-none d-xl-block ">
-          <a href="#">Features</a>
+
+
+        <Col xl={2} className="features-link d-none d-xl-block ">
+          <BootstrapNav.Link href="#">Features</BootstrapNav.Link>
         </Col>
+
+        
         <Col xs={6} xl={8}>
           <Row className="justify-content-end align-items-center">
-            <label
-              htmlFor="flexSwitchCheckDefault"
-              className="dark-mode-text d-none col-4 d-md-block text-md-end col-xl-2"
-              aria-label="darkmode switch"
-            >
-              Dark Mode
-            </label>
-            <Col xs={3} xl={2}>
-              <FormCheck
-                className="form-check-input"
-                type="switch"
-                id="flexSwitchCheckDefault"
-              />
-            </Col>
+            <DarkModeSwitch />
             <Col xs={2} className="d-xl-none">
-                <Button className="navbar-toggle ">
-                  <span className="navbar-toggle"></span>
-                </Button>
+              <Navbar.Toggle
+                aria-controls="basic-navbar-nav"
+                onClick={() => setExpanded(!expanded)}
+              ></Navbar.Toggle>
             </Col>
             <Col xl={2} className="d-none d-xl-block">
-            <PrimaryButton 
-            className="sign-btn transition"
-            text="Sign in / up"
-            />
+              <PrimaryButton
+                className="sign-btn transition"
+                text="Sign in / up"
+              />
             </Col>
           </Row>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <BootstrapNav className="ms auto d-xl-none">
+              <BootstrapNav.Link href="#" className="features-link">Features</BootstrapNav.Link>
+              <Col className="d-xl-none">
+                <PrimaryButton
+                  className="sign-btn transition"
+                  text="Sign in / up"
+                />
+              </Col>
+            </BootstrapNav>
+          </Navbar.Collapse>
         </Col>
       </Navbar>
     </Row>
