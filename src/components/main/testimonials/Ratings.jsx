9ad4f7f55f4852eb60569136b240cb737e2215cb
Mona Khorasani <monakhrsn@gmail.com>
2024-10-28 08:22:33 +0100

    Fix styling for unfilled star

--- src/components/main/testimonials/Ratings.jsx
+++ src/components/main/testimonials/Ratings.jsx
@@ -5,8 +5,8 @@ const Ratings = ({ rating }) => {
   const totalStars = 5;
   const stars = [];
 
-  for(let i = 1 ; i <= totalStars; i++)
-      stars.push(<Rating key={i} isFilled={i <= rating} />)
+  for (let i = 1; i <= totalStars; i++)
+    stars.push(<Rating key={i} isFilled={i <= rating} />);
 
   return <div className="rating">{stars}</div>;
 };
