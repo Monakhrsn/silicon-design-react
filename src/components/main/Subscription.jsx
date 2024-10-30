a4481fadb4b8c4d3f0094551e264d596e12eed54
Mona Khorasani <monakhrsn@gmail.com>
2024-10-30 10:19:06 +0100

    Develop email validation with regex, change handler and working on subit handler

--- src/components/main/Subscription.jsx
+++ src/components/main/Subscription.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   Container,
   Col,
@@ -8,7 +9,57 @@ import {
   Form,
 } from "react-bootstrap";
 
-const Subscription = () => {
+const Subscription = (e) => {
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [loading, setLoading] = useState(true)
+
+  const isValidEmail =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const changeHandler = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+    setIsValid(newEmail.toLowerCase().match(isValidEmail));
+  };
+
+  const submitHandler = async(e) => {
+    e.preventDefault();
+
+    if (!isValid) {
+      return alert(`Emai address you have insert i not ${email}`);
+    }
+
+
+    // try {
+    //   const res = await fetch(
+    //     "https://win24-assignment.azurewebsites.net/api/forms/subscribe"
+    //   );
+
+    //   if (!res.ok) {
+    //     throw new Error(`An Error occured! Status: ${res.status}`);
+    //   }
+
+    //   const fetchResponse = await res.json();
+    //   console.log(fetchResponse)
+
+    //   setData(fetchResponse);
+    //   setLoading(false);
+    // } catch (err) {
+    //   setError(err);
+    //   setLoading(false);
+    // }
+
+  
+    if (loading) {
+      return <div>Loading.. .</div>
+    }
+  
+    if (error) {
+      return <div>Error: {error.message}</div>
+    }
+  };
+
   return (
     <section className="pt-md-4">
       <Container>
@@ -39,22 +90,36 @@ const Subscription = () => {
                 <Col md={6}>
                   <Row>
                     <Col className="d-flex justify-content-start align-items-center px-5">
-                      <InputGroup className="input-wrapper mb-4 mb-md-0">
-                        <Form.Control
-                          type="text"
-                          className="form-control email"
-                          placeholder="Your Email"
-                          aria-label="Recipient's username"
-                          aria-describedby="button-addon2"
-                        />
-                        <Button
-                          className="btn btn-primary transition"
-                          type="button"
-                          id="button-addon2"
-                        >
-                          Subscribe
-                        </Button>
-                      </InputGroup>
+                      <Form onSubmit={submitHandler}>
+                        <InputGroup className="input-wrapper mb-4 mb-md-0">
+                          <Form.Control
+                            type="email"
+                            className={`form-control email ${
+                              isValid ? "" : "is-invalid"
+                            }`}
+                            placeholder="Your Email"
+                            aria-label="Recipient's username"
+                            aria-describedby="button-addon2"
+                            value={email}
+                            onChange={(e) => changeHandler(e)}
+                            required
+                          />
+
+                          <Button
+                            className="btn btn-primary transition"
+                            type="submit"
+                            id="button-addon2"
+                            disabled={!isValid}
+                          >
+                            Subscribe
+                          </Button>
+                          {!isValid && (
+                            <div className="invalid-feedback">
+                              Please enter a valid email address.
+                            </div>
+                          )}
+                        </InputGroup>
+                      </Form>
                     </Col>
                   </Row>
                 </Col>
